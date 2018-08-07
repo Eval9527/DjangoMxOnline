3e862451ba357a8bcf996ef7aee541a6ca79ef86
Pooc <pooc95@163.com>
2018-08-07 09:47:26 +0800

    8-3 课程详情页

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -2,9 +2,12 @@
 
 from django.conf.urls import url
 
-from .views import CourseList
+from .views import CourseList, CourseDetailView
 
 urlpatterns = [
     # 课程列表
     url(r'^list/$', CourseList.as_view(), name="course_list"),
+
+    # 课程详情页
+    url(r'^detail/(?P<course_id>\d+)/$', CourseDetailView.as_view(), name="course_detail"),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -4,6 +4,7 @@ from django.views.generic import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Course
+from operation.models import UserFavorite
 # Create your views here.
 
 
@@ -34,4 +35,38 @@ class CourseList(View):
             'all_courses': courses,
             'sort': sort,
             'hot_courses': hot_courses,
+        })
+
+
+class CourseDetailView(View):
+    """
+    课程详情页
+    """
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+
+        # 增加课程点击数
+        course.click_nums += 1
+        course.save()
+
+        # 用户收藏和取消收藏课程、机构
+        has_fav_course = False
+        has_fav_org = False
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.id, fav_type=1):
+                has_fav_course = True
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.course_org.id, fav_type=2):
+                has_fav_org = True
+
+        # 通过相同标签进行推荐
+        tag = course.tag
+        if tag:
+            relate_courses = Course.objects.filter(tag=tag)[1:3]
+        else:
+            relate_courses = []
+        return render(request, "course-detail.html", {
+            'course': course,
+            'relate_courses': relate_courses,
+            'has_fav_course': has_fav_course,
+            'has_fav_org': has_fav_org,
         })
